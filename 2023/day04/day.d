2fb07c1ea249ed891a1ee7d268582cba9c47fb74
43_50ph14 <so_s@gmx.de>
2023-12-04 22:29:52 +0100

    D

---
+++ 2023/day04/day.d
@@ -0,0 +1,53 @@
+import std.stdio;
+import std.file : readText;
+import std.algorithm;
+import std.range;
+import std.conv;
+import std.string;
+import std.format;
+import std.ascii;
+
+void main(string[] args)
+{
+    auto input = readText(args[1]).split('\n');
+    while(input[$-1].length == 0)
+    {
+        input.popBack;
+    }
+
+    int points;
+    int[] counts = new int[input.length];
+    counts[] = 1;
+
+    foreach(i, line; input)
+    {
+        auto carddata = line[line.indexOf(':') + 1 .. $].split('|').map!(s => s.split.map!(to!int).array).array;
+        bool[int] numbers;
+        foreach(v; carddata[1])
+        {
+            numbers[v] = true;
+        }
+        int wn = 0;
+        foreach(v; carddata[0])
+        {
+            if(v in numbers)
+            {
+                ++wn;
+            }
+        }
+
+        if(wn > 0)
+        {
+            points += 1 << (wn - 1);
+            foreach(n; i + 1 .. i + 1 + wn)
+            {
+                if(n < counts.length)
+                {
+                    counts[n] += counts[i];
+                }
+            }
+        }
+    }
+    writeln(points);
+    writeln(sum(counts));
+}
